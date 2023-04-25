ba4df805c153477477be570713f5eb3209637260
Matheus <theusma58@gmail.com>
2023-04-25 15:18:16 -0300

    navbar

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, redirect, request
+
+app = Flask(__name__)
+app.secret_key = '1234321'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
