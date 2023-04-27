8d253b5b3468e9c3ed263d1732986e56a587db99
Kaeix <rodrigoviclima@gmail.com>
2023-04-27 05:35:27 -0300

    Quarta

--- static/script.js
+++ static/script.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
-    $(".dropdown-toggle").dropdown();
-});
+
+
+// ativar todos os data-anime //
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
+
+  item.forEach((element) => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add("animate");
+    } else {
+      element.classList.remove("animate");
+    }
+  });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", ()=>{
+  animeScroll();
+})
